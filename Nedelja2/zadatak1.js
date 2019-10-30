8637128e1aecc6c2b933457ee2f2f37b60c60e18
mionam <momcilovicmiona@gmail.com>
2019-10-30 16:13:24 +0100

    Uradjen prvi zadatak.

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,17 @@
+var a=3;
+var b=8;
+var addition=a+b;
+var substraction=a-b;
+var multiplication=a*b;
+var division=b/a;
+var modulus=b%a;
+var degree=a^b;
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+console.log(degree);
+
+
+

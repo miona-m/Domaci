37ce7809f88f184bce49e3a1c8d4c0ec8f66f7fc
mionam <momcilovicmiona@gmail.com>
2019-10-30 17:09:59 +0100

    Urađen drugi zadatak.

---
+++ Nedelja2/zadatak2.js
@@ -0,0 +1,4 @@
+
+var titleChange="Welcome to Dom";
+var title=document.getElementById('title').innerHTML=(titleChange);
+var divAddition=document.getElementById('newdiv').innerHTML=('Ovo je naknadno ubacen div.');

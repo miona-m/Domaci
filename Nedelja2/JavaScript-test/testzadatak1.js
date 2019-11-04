1a0828f56bfe710d799a0ea0b5f35ae0d86c594d
mionam <momcilovicmiona@gmail.com>
2019-11-04 13:06:43 +0100

    Prvi test.

---
+++ Nedelja2/JavaScript-test/testzadatak1.js
@@ -0,0 +1,20 @@
+//alternativni
+let k=5;
+let n=6;
+let kol=0;
+let sum1=0;
+let sum2=0;
+
+for (var d=1; d<=n; d++){
+    sum1+=d+1;
+}
+console.log(sum1);
+
+for (var a=0; a<=n; a++){
+    sum2+=(k+a);
+}
+console.log(sum2);
+
+kol=sum1/sum2;
+console.log(kol);
+
---
+++ Nedelja2/JavaScript-test/testzadatak4.js
@@ -0,0 +1,1 @@
+let mesec=4;
